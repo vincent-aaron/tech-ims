3f005520bc84a233facca12b60c705a9a2ff1ad8
vincent-aaron7 <vincent4aron@gmail.com>
2026-06-22 09:48:36 +0800

    updated setting.py

--- techims/settings.py
+++ techims/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&@0_idb)gm4#$*)77w7qti5$1*kkqf7pz8n92f4l)^qri80+f*'
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-key")
+#'django-insecure-&@0_idb)gm4#$*)77w7qti5$1*kkqf7pz8n92f4l)^qri80+f*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
